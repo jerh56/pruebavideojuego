2ea8d0588fa7dbe4d83be9235684324e5d487d3f
Alumno <Alumno@10.76.179.133>
2018-03-03 13:45:50 -0600

    Revert "Merge origin/master"
    This reverts commit 57e53421393bbde711b089f5ecbf92c0b148db97.

--- pruebavideojuego3/src/pruebavideojuego3/Pruebavideojuego3.java
+++ pruebavideojuego3/src/pruebavideojuego3/Pruebavideojuego3.java
@@ -17,7 +17,6 @@ public class Pruebavideojuego3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
-        System.out.println("Hola Mundo");
     }
     
 }
