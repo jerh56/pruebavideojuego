31466d648977087df82c830889ac1d1eff0a5378
Alumno <Alumno@10.76.179.133>
2018-03-03 13:50:37 -0600

    Merge origin/master
    Conflicts:
    	src/pruebapacman/Principal.java

--- pruebavideojuego3/src/pruebavideojuego3/Pruebavideojuego3.java
+++ pruebavideojuego3/src/pruebavideojuego3/Pruebavideojuego3.java
@@ -17,6 +17,7 @@ public class Pruebavideojuego3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo");
     }
     
 }
